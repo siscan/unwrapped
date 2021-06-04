f4c36853bc00f6df9e082bd85aa9f5122fafff8a
Kathleen Ashley <ashleyk@oregonstate.edu>
2021-06-05 01:28:42 +0700

    Renewed foodData.json

--- server.js
+++ server.js
@@ -1,35 +1,31 @@
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars')
-
-//var foodData = require('./foodData.json')
+var foodData = require('./foodData.json')
 
 var app = express();
-
-var port = 8080;
-
-app.engine('handlebars', exphbs({ defaultLayout: null }))
+app.engine('handlebars', exphbs({ defaultLayout: main }))
 app.set('view engine', 'handlebars')
+
+var port = 3000;
+
+app.get('/recipe', function (req, res, next) {
+    res.status(200).render('recipesPage', {
+			cards: foodData
+		})
+});
 
 app.use(express.static('public'));
 
-
-
-app.get('/recipe', function (req, res, next) {
-
-
-
-    res.status(200).render('recipesPage', {cards: [1, 2, 3, 4]})
-
-
-});
-
+/*
 app.get("*", function (req, res, next) {
   res.status(404).render('404', {
     page: req.url,
     scripts: [ "/index.js" ]
   });
 });
+
+*/
 
 app.listen(port, function (err) {
   if (err) {
