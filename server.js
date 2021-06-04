5288c6424c5cb12cdec7e51c13524dfd381ca993
siscan <siscan@oregonstate.edu>
2021-06-04 06:08:53 -0700

    commented out json so it boots

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars')
 
-var foodData = require('./foodData.json')
+//var foodData = require('./foodData.json')
 
 var app = express();
 
