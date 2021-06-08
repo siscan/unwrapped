e10e870c63846ead9c67727944991bf632f07f4e
DevinBright <72934653+DevinBright@users.noreply.github.com>
2021-06-07 20:08:09 -0700

    Added builder to recipes page and made it handlebars

--- public/recipeCardTemplate.js
+++ public/recipeCardTemplate.js
@@ -8,14 +8,20 @@ templates['recipeCard'] = template({"compiler":[8,">= 4.3.0"],"main":function(co
         return undefined
     };
 
-  return "<article class=\"recipe-card\">\n  <div class=\"img-container\">\n    <!-- The <i> tag below includes the bullhorn icon from Font Awesome.  Do not directly style this element. -->\n    <img class=\"recipe-img\" src=\""
+  return "<section class=\"recipe-card\">\r\n  <div class=\"img-container\">\r\n    <!-- The <i> tag below includes the bullhorn icon from Font Awesome.  Do not directly style this element. -->\r\n    <img class=\"recipe-img\" src=\""
     + alias4(((helper = (helper = lookupProperty(helpers,"url") || (depth0 != null ? lookupProperty(depth0,"url") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"url","hash":{},"data":data,"loc":{"start":{"line":4,"column":33},"end":{"line":4,"column":40}}}) : helper)))
-    + "\"/>\n  </div>\n  <div class=\"recipe-title\">\n    <a href=\"#\">"
+    + "\"/>\r\n  </div>\r\n  <div class=\"recipe-title\">\r\n    <a href=\"#\">"
     + alias4(((helper = (helper = lookupProperty(helpers,"title") || (depth0 != null ? lookupProperty(depth0,"title") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data,"loc":{"start":{"line":7,"column":16},"end":{"line":7,"column":25}}}) : helper)))
-    + "</a>\n  </div>\n  <div class=\"recipe-time\">\n    <i class=\"fas fa-clock\"></i> Time: "
+    + "</a>\r\n  </div>\r\n  <div class=\"recipe-time\">\r\n    <i class=\"fas fa-clock\"></i> Time: "
     + alias4(((helper = (helper = lookupProperty(helpers,"duration") || (depth0 != null ? lookupProperty(depth0,"duration") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"duration","hash":{},"data":data,"loc":{"start":{"line":10,"column":39},"end":{"line":10,"column":51}}}) : helper)))
-    + " mins\n  </div>\n  <div class=\"recipe-servings\">\n    <p><i class=\"fas fa-user-friends\"></i> serves "
+    + " mins\r\n  </div>\r\n  <div class=\"recipe-servings\">\r\n    <p><i class=\"fas fa-user-friends\"></i> serves "
     + alias4(((helper = (helper = lookupProperty(helpers,"serving") || (depth0 != null ? lookupProperty(depth0,"serving") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"serving","hash":{},"data":data,"loc":{"start":{"line":13,"column":50},"end":{"line":13,"column":61}}}) : helper)))
-    + " </p>\n    <p class=\"recipe-favorite\"><i class=\"fas fa-heart\"></i></p>\n  </div>\n</article>\n";
+    + " </p>\r\n    <p class=\"recipe-favorite\"><i class=\"fas fa-heart\"></i></p>\r\n  </div>\r\n  <div class=\"recipe-ingredients hidden\">\r\n    "
+    + alias4(((helper = (helper = lookupProperty(helpers,"ingredients") || (depth0 != null ? lookupProperty(depth0,"ingredients") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"ingredients","hash":{},"data":data,"loc":{"start":{"line":17,"column":4},"end":{"line":17,"column":19}}}) : helper)))
+    + "\r\n  </div>\r\n  <div class=\"recipe-instructions hidden\">\r\n    "
+    + alias4(((helper = (helper = lookupProperty(helpers,"instructions") || (depth0 != null ? lookupProperty(depth0,"instructions") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"instructions","hash":{},"data":data,"loc":{"start":{"line":20,"column":4},"end":{"line":20,"column":20}}}) : helper)))
+    + "\r\n  </div>\r\n  <div class=\"recipe-author hidden\">\r\n    "
+    + alias4(((helper = (helper = lookupProperty(helpers,"author") || (depth0 != null ? lookupProperty(depth0,"author") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"author","hash":{},"data":data,"loc":{"start":{"line":23,"column":4},"end":{"line":23,"column":14}}}) : helper)))
+    + "\r\n  </div>\r\n</section>\r\n";
 },"useData":true});
 })();
